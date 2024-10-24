6fb0a769d5e77ee32c54e21be96dfeed5af3a318
Daffa954 <dkhoirul05@gmail.com>
2024-10-24 10:42:15 +0700

    update
    nama

--- app/src/main/java/com/example/week6_github/MainActivity.kt
+++ app/src/main/java/com/example/week6_github/MainActivity.kt
@@ -36,6 +36,15 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name! hello wrold",
         modifier = modifier
     )
+
+    Text(
+        text = "Hello $name! hello wrold",
+        modifier = modifier
+    )
+    Text(
+        text = "Hello $name! hello wrold",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
